24ea17ecaf3742b82eb991eec911092c6cd74c78
Thales Drumond <talito-swite@hotmail.com>
2022-10-20 01:31:36 -0300

    Finish

--- src/app/registerform/registerform.component.ts
+++ src/app/registerform/registerform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registerform',
@@ -8,8 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterformComponent implements OnInit {
 
   constructor() { }
-
+  registerForm:any; //form
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      "firstName": new FormControl(null,[Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      "lastName": new FormControl(null,[Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      "emailId": new FormControl(null,[Validators.required,Validators.email]),
+      "mobileNumber": new FormControl(null,[Validators.required,Validators.pattern('[0-9]*')])
+    });
+  }
+ 
+  //subimit function 
+  submitData() {
+    console.log(this.registerForm.value);
+
+    if(this.registerForm.valid){
+
+      alert(`Obrigado ${this.registerForm.value.firstName}`);
+      this.registerForm.reset(); //reset form value
+    }
+
   }
 
+  get firstname() { return this.registerForm.get('firstName'); } //firstName
+  get lastname() { return this.registerForm.get('lastName'); } //lastName
+  get emailid() {return this.registerForm.get('emailId'); } //emailid
+  get mobilenumber() { return this.registerForm.get('mobileNumber'); } //mobilenumber
 }
